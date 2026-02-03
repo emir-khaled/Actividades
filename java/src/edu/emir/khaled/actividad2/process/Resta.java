11ca46fc380e81275fb0e7d1ac6d6f07b5fc6d5d
Emir Khaled Lopez <96266655+emir-khaled@users.noreply.github.com>
2026-02-02 23:39:24 -0600

    Limpiar rama actividad3: solo archivos .java de actividad3

--- java/src/edu/emir/khaled/actividad2/process/Resta.java
+++
@@ -1,8 +0,0 @@
-package edu.emir.khaled.actividad2.process;
-// Clase para realizar operaciones de resta
-
-public class Resta {
-    public double calcular(double a, double b) {
-        return a - b;
-    }
-}
--- java/src/edu/emir/khaled/actividad2/process/Suma.java
+++
@@ -1,8 +0,0 @@
-package edu.emir.khaled.actividad2.process;
-// Clase para realizar operaciones de suma
-
-public class Suma {
-    public double calcular(double a, double b) {
-        return a + b;
-    }
-}
